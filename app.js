dc72e3a14aa10bf6dba405aa906e625ea97a97ba
chinara mammadova <cmmmdova739@gmail.com>
2024-10-01 13:46:16 +0000

    btn

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 "use strict";
 $(document).ready(function () {
 
+    $('.btn').click(function (e) { 
+        e.preventDefault();
+        if(!$.trim($('input').val())==" "){
+            let a = c => {
+                for(let i = 0 ; i<c; i++){$('.minibox').append(`<h2>${c}</h2>`)}
+            }
+            let v = f =>Number(f)? a(f): $('.minibox').append(`<h2>${f}</h2>`);
+            a($('input').val())
+        }
+        $('input').val(' ')
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
